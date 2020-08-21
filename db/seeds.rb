fa2f6a7efff82729cf6636795a62bf3e2b28c30b
Tiago Palhoca <palhoca.tiago@gmail.com>
2020-08-21 15:10:39 +0100

    akmsk

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+
+Cocktail.create(name: "Sex on the Beach")
+Cocktail.create(name: "Levanta-Mortos")
+Cocktail.create(name: "B-52")
+
